16252f8a197d853a8356300d7d616a003ae77553
Mizamii <yasminbertoni7@gmail.com>
2026-07-30 19:10:20 -0300

    correção de pequenos erros

--- server.js
+++ server.js
@@ -849,8 +849,16 @@ app.post("/esqueceu-senha", async (req, res) => {
 ========================== */
 
 app.post("/cadastro-finalizar", async (req, res) => {
+    
 
     const dados = req.session.cadastro;
+
+    if (dados.tipo === "pai") {
+        console.log("Tipo recebido:", dados.tipo);}
+
+    //mudar aq
+    console.log("DADOS DA SESSÃO:");
+    console.log(dados);
 
     if (!dados) {
         return res.status(400).json({
